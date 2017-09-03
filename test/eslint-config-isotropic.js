e7c307a3670c68f77ff90faae888ac6728d9aeca
Steven Olmsted <steven.olmsted@ibigroup.com>
2017-09-03 17:40:31 -0400

    Update tests

--- test/eslint-config-isotropic.js
+++ test/eslint-config-isotropic.js
@@ -1,22 +1,29 @@
 import * as eslintConfigIsotropic from '../js/eslint-config-isotropic.js';
+import chai from 'chai';
+import mocha from 'mocha';
 
-import {
-    describe,
-    it
-} from 'mocha';
+mocha.describe('eslint-config-isotropic', () => {
+    mocha.it('should be a shared eslint config object', () => {
+        chai.expect(eslintConfigIsotropic).to.be.an('object');
+        chai.expect(eslintConfigIsotropic).not.to.have.property('ecmaFeatures');
+        chai.expect(eslintConfigIsotropic).not.to.have.property('env');
+        chai.expect(eslintConfigIsotropic).to.have.property('globals').that.is.an('object');
+        chai.expect(eslintConfigIsotropic).not.to.have.property('parserOptions');
+        chai.expect(eslintConfigIsotropic).not.to.have.property('root');
+        chai.expect(eslintConfigIsotropic).to.have.property('rules').that.is.an('object');
 
-import {
-    expect
-} from 'chai';
+        Object.keys(eslintConfigIsotropic.rules).forEach(ruleName => {
+            let setting = eslintConfigIsotropic.rules[ruleName];
 
-describe('eslint-config-isotropic', () => {
-    it('should be a shared eslint config object', () => {
-        expect(eslintConfigIsotropic).to.be.an('object');
-        expect(eslintConfigIsotropic).not.to.have.property('ecmaFeatures');
-        expect(eslintConfigIsotropic).not.to.have.property('env');
-        expect(eslintConfigIsotropic).to.have.property('globals').that.is.an('object');
-        expect(eslintConfigIsotropic).not.to.have.property('parserOptions');
-        expect(eslintConfigIsotropic).not.to.have.property('root');
-        expect(eslintConfigIsotropic).to.have.property('rules').that.is.an('object');
+            if (Array.isArray(setting)) {
+                setting = setting[0];
+            }
+
+            chai.expect(setting).to.be.oneOf([
+                'error',
+                'off',
+                'warn'
+            ]);
+        });
     });
 });
